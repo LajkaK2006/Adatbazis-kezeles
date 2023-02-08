a2a4e46f00d203b5ec34eb0763dc0eb6179067ac
lajkak <92091234+LajkaK2006@users.noreply.github.com>
2023-02-08 11:15:18 +0100

    Add files via upload

---
+++ osztaly_nevsor.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Feb 08. 11:15
+-- Kiszolgáló verziója: 10.4.20-MariaDB
+-- PHP verzió: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `rendszergazda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `osztaly_nevsor`
+--
+
+CREATE TABLE `osztaly_nevsor` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `vnev` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
+  `knev` varchar(50) COLLATE utf8_hungarian_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `osztaly_nevsor`
+--
+
+INSERT INTO `osztaly_nevsor` (`id`, `vnev`, `knev`) VALUES
+(1, 'Barcsak', 'Vinzent'),
+(2, 'Borbely', 'Gergely'),
+(3, 'Csordas', 'Csenge'),
+(4, 'Enyedi', 'Sandor'),
+(5, 'Gal', 'Mate'),
+(6, 'Gyorgy ', 'Botond'),
+(7, 'Hering', 'Mate'),
+(8, 'Juhasz', 'Bence'),
+(9, 'Kreipos', 'Gabor'),
+(10, 'Lajka', 'Kristof'),
+(11, 'Nemes', 'Alexandra'),
+(12, 'Skarufa', 'Akos'),
+(13, 'Szabo', 'Gergely'),
+(14, 'Varadi ', 'Adam');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `osztaly_nevsor`
+--
+ALTER TABLE `osztaly_nevsor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `osztaly_nevsor`
+--
+ALTER TABLE `osztaly_nevsor`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
